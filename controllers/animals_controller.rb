1497b306d9359f3a0128d4b6aaa163b069005671
annalanigan <annalanigan@outlook.com>
2017-11-29 10:55:28 +0000

    added a delete animal function (with check page)

--- controllers/animals_controller.rb
+++ controllers/animals_controller.rb
@@ -5,40 +5,53 @@ require( 'pry-byebug' )
 require_relative('../models/owner')
 require_relative('../models/animal')
 
-#index see all animals
+# INDEX see all animals
 get '/animals' do
   @animals = Animal.all
   erb ( :"animals/index" )
 end
 
-#get form to create new animal
+#get form to CREATE new animal
 get '/animals/new' do
   @animals = Animal.all
   erb ( :"animals/new" )
 end
 
-# create new animal
+# create NEW animal
 post '/animals' do
   @animal = Animal.new(params)
   @animal.save
   redirect to '/animals'
 end
 
-# to see details of an individual animals
+# to SHOW details of an individual animals
 get '/animals/:id' do
   @animal = Animal.find(params[:id])
   erb ( :"animals/show" )
 end
 
-# get form to update animal
+# get form to EDIT animal
 get '/animals/:id/edit' do
   @animal = Animal.find(params[:id])
   @owners = Owner.all
   erb ( :"animals/edit" )
 end
 
-# update animal
+# UPDATE animal
 put '/animals/:id' do
   Animal.new( params ).update
+  redirect to '/animals'
+end
+
+# confirm delete animal
+get '/animals/:id/delete' do
+  @animal = Animal.find(params[:id])
+  erb (:"animals/delete")
+end
+
+# DELETE animal
+delete '/animals/:id' do
+  animal = Animal.find(params[:id])
+  animal.delete
   redirect to '/animals'
 end
